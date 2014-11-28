e7bea6128d6fa8fbc3822dc60dc4ba9a46ee7400
joseluis <joseluis.santos.cs@gmail.com>
2014-11-28 10:48:11 +0100

    GET and POST calls

--- DS_server.js
+++ DS_server.js
@@ -6,9 +6,9 @@ var connect = require('connect')
     , path = require('path')
     , mongoose = require('mongoose')
     , db = require("./routes/db.js")
-    //, NanoTimer = require('nanotimer')
-    //, port = (process.env.PORT || )
-    , port = 8080
+//, NanoTimer = require('nanotimer')
+//, port = (process.env.PORT || )
+    , port = 8082
     , ipadress = process.env.host
     , context = "/datastore"
     , logger = require('morgan')
@@ -26,23 +26,23 @@ var connect = require('connect')
 var app = express();
 
 //server.configure(function(){
-    app.set('port', 8080);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.set('view options', { layout: false });
-    app.use(logger('dev'));
-  //  app.use(connect.bodyParser());
-    app.use(session({ resave: true,
-                      saveUninitialized: true,
-                      secret: 'uwotm8'
-    }));
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.text({limit: '50mb'}));
-    app.use(bodyParser.json({limit: '50mb'}));
-    app.use(context, express.static(__dirname + '/static'));
-    //app.use(express.static(__dirname + '/static'));
+app.set('port', 8082);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.use(logger('dev'));
+//  app.use(connect.bodyParser());
+app.use(session({ resave: true,
+    saveUninitialized: true,
+    secret: 'uwotm8'
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(context, express.static(__dirname + '/static'));
+//app.use(express.static(__dirname + '/static'));
 
-    //server.use(server.router);
+//server.use(server.router);
 //});
 //server.listen( port, ipadress);
 //  app.listen(port);
@@ -87,10 +87,10 @@ ioWeb.on('connection', function(socket){
 
 app.get(path.join(context,'/'), routes.index);
 
-app.get(path.join(context, '/data'), getAllEventsJSONP);
+app.get(path.join(context, '/events'), getAllEventsJSONP);
 
 
-app.post(path.join(context, '/upload'), storeEvent);
+app.post(path.join(context, '/event'), storeEvent);
 
 //console.log('Listening on http://' + server.address() + ':' + port );
 
@@ -114,8 +114,7 @@ function getAllEventsJSONP (request, response, next) {
         });
 
     } else {
-
-        var query = db.queryAllWithFilter(request.query.username, request.query.verb, request.query.starttime, request.query.endtime, request.query.object, request.query.context);
+        var query = db.queryAllWithFilter(request.query.username, request.query.verb, request.query.starttime, request.query.endtime, request.query.target, request.query.object, request.query.context);
         var promise = query.exec();
         promise.onResolve(function (err, results) {
             if (err)
@@ -160,91 +159,91 @@ function storeEvent(request, response) {
 
 
 /*
-function storeFile(request, response) {
-    console.log(request.body);
+ function storeFile(request, response) {
+ console.log(request.body);
 
-   try {
+ try {
 
-        //var received = JSON.parse(request.body);
-        var received = request.body;
+ //var received = JSON.parse(request.body);
+ var received = request.body;
 
-        var image = new db.Image();
-        image.imageName = received.imageName;
-        image.tripID = received.tripID;
-        image.userID = received.userID;
-        //image.raw = received.raw;
+ var image = new db.Image();
+ image.imageName = received.imageName;
+ image.tripID = received.tripID;
+ image.userID = received.userID;
+ //image.raw = received.raw;
 
-        if (received.userID !== undefined) {
+ if (received.userID !== undefined) {
 
-            image.save(function (err, image) {
-                if (err) {
+ image.save(function (err, image) {
+ if (err) {
 
-                    return console.error(err);
-                    response.json({"Sorry. Failed to upload. Did you include all info?":400});
+ return console.error(err);
+ response.json({"Sorry. Failed to upload. Did you include all info?":400});
 
-                } else {
-                    var save2db = true;
-                    //if this succeeds, save to disk and add automatically to the tripDATA
-                    //fs.writeFile("~/node-apps/quantifiedBike/uploads/" + image._id + "_" + received.imageName, new Buffer(received.raw, 'base64'), function(err) {
-                    fs.writeFile("./uploads/" + image._id + "_" + received.imageName, new Buffer(received.raw, 'base64'), function(err) {
-                        if(err) {
-                            console.log(err);
-                            response.json({"Upload failed":400});
-                            save2db = false;
+ } else {
+ var save2db = true;
+ //if this succeeds, save to disk and add automatically to the tripDATA
+ //fs.writeFile("~/node-apps/quantifiedBike/uploads/" + image._id + "_" + received.imageName, new Buffer(received.raw, 'base64'), function(err) {
+ fs.writeFile("./uploads/" + image._id + "_" + received.imageName, new Buffer(received.raw, 'base64'), function(err) {
+ if(err) {
+ console.log(err);
+ response.json({"Upload failed":400});
+ save2db = false;
 
-                        } else {
-                            console.log("The file was saved!");
-                        }
-                    });
+ } else {
+ console.log("The file was saved!");
+ }
+ });
 
-                     if (save2db){
-                        db.Trip.findById(received.tripID, function (err, trip) {
-                            if (err) {
-                                image.tripID = undefined;
-                                image.save();
-                                console.log("damnit! image without existing tripID");
-                            }
+ if (save2db){
+ db.Trip.findById(received.tripID, function (err, trip) {
+ if (err) {
+ image.tripID = undefined;
+ image.save();
+ console.log("damnit! image without existing tripID");
+ }
 
-                            var dateOfImage;
-                            if (received.timestamp !== undefined) {
-                                dateOfImage = received.timestamp;
-                            } else {
-                                dateOfImage = new Date();
-                            }
+ var dateOfImage;
+ if (received.timestamp !== undefined) {
+ dateOfImage = received.timestamp;
+ } else {
+ dateOfImage = new Date();
+ }
 
-                            var el = {
-                                "sensorID": 8,   //Check http://ariadne.cs.kuleuven.be/wiki/index.php/PEnO3-1415
-                                "timestamp": dateOfImage,
-                                "data": [image._id + "_" + received.imageName]  //free to do anything
-                            };
+ var el = {
+ "sensorID": 8,   //Check http://ariadne.cs.kuleuven.be/wiki/index.php/PEnO3-1415
+ "timestamp": dateOfImage,
+ "data": [image._id + "_" + received.imageName]  //free to do anything
+ };
 
-                            trip.sensorData.push(el);
+ trip.sensorData.push(el);
 
-                            //save to database
-                            trip.save(function (err) {
-                                if (err) return console.error(err);
-                                else
-                                    console.log(JSON.stringify(trip));
-                            });
-                        });
-                        response.json({"Upload succeeded":200, "image_id":image._id});
-                     }
-                }
-
-
-            });
+ //save to database
+ trip.save(function (err) {
+ if (err) return console.error(err);
+ else
+ console.log(JSON.stringify(trip));
+ });
+ });
+ response.json({"Upload succeeded":200, "image_id":image._id});
+ }
+ }
 
 
-        }
-
-    } catch (e) {
-
-        console.log("Illegal JSON data received.");
-        response.json({"Sorry. Failed to upload. Did you include all info?":400});
-
-    }
+ });
 
 
+ }
 
-}
+ } catch (e) {
+
+ console.log("Illegal JSON data received.");
+ response.json({"Sorry. Failed to upload. Did you include all info?":400});
+
+ }
+
+
+
+ }
  */
